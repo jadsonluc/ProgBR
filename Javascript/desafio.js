9eb6a939a054d11b0dff0f70ed78e78eb4cdf21f
Jadson Lucas <106039973+Jadsonluc@users.noreply.github.com>
2022-07-11 15:06:55 -0300

    desafio.js

--- Javascript/desafio.js
+++ Javascript/desafio.js
@@ -1,7 +1,35 @@
 //Nome do aluno - nota1 - nota2 - media - Aprovado
 
+var nomes = ['Igor', 'José', 'Maria']
+var notasA = [5, 5.5, 9]
+var notasB = [7, 9, 5]
+
+function media(n1, n2){
+    return (n1 + n2 ) / 2
+}
 
 
+
+function passou(media) {
+    if (media >= 7) {
+        return "Aprovado"
+    } else {
+        return "Reprovado"
+    }
+}
+for (var index in nomes) {
+
+    var nota1 = notasA[index]
+    var nota2 = notasB[index]
+    var alunos = nomes[index]
+    var m = media(nota1, nota2)
+    console.log(alunos + " - " +
+                nota1 + " - " +
+                nota2 + " - " +
+                media(nota1, nota2) + " - " +
+                passou(m)
+    ) 
+}
 
 
 
