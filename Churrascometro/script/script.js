12c582eca84060f11bf7a76639ff96b835d8644d
Jadson Lucas <106039973+Jadsonluc@users.noreply.github.com>
2022-07-15 11:08:04 -0300

    ok

--- Churrascometro/script/script.js
+++ Churrascometro/script/script.js
@@ -1,1 +1,15 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Carne - 400gr por pessoa + 6 horas - 650
+Cerveja - 1200ml por pessoa + 6 horas - 2000ml
+Refrigerante/agua - 1000ml por pessoa + 6 horas 1500ml
+Crianças valem 0,5
+*/ 
+
+let inputAdultos = document.getElementById("adultos")
+let input
+
+
+
+
+
+
