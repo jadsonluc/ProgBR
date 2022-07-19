607532e6ec9d5c3dbe4b8ab072f0cd372a79b2e8
Jadson Lucas <106039973+Jadsonluc@users.noreply.github.com>
2022-07-19 12:52:10 -0300

    ok

--- Churrascometro/script/script.js
+++ Churrascometro/script/script.js
@@ -6,7 +6,31 @@ Crianças valem 0,5
 */ 
 
 let inputAdultos = document.getElementById("adultos")
-let input
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular() {
+    let adultos = inputAdultos.value 
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
+    let duracaoMaior = (adultos * 650) + (650/2 * criancas)
+    let duracaoMenor = (adultos * 400) + (400/2 * criancas)
+    let duracaoMaiorCerveja = (adultos * 2000) 
+    let duracaoMenorCerveja = (adultos * 1200) 
+    let duracaoMaiorBebida = (adultos * 1500) + (1500/2 * criancas)
+    let duracaoMenorBebida = (adultos * 1000) + (1000/2 * criancas)
+if (duracao >= 6) {
+    resultado.innerHTML = `${duracaoMaior/1000}Kg de Carne, <br>
+                           ${Math.ceil(duracaoMaiorCerveja/355)} latas de Cerveja, <br>
+                           ${duracaoMaiorBebida/1000} litros de Bebidas.`
+    
+} else {
+    resultado.innerHTML = `${duracaoMenor/1000}Kg de Carne, <br>
+                           ${Math.ceil(duracaoMenorCerveja/355)} latas de Cerveja, <br>
+                           ${duracaoMenorBebida/1000} litros de Bebidas.`
+}}
 
 
 
